e12d6380024cbe066c1878ce5618f1fbe3e6b603
bintangjtobing <hello@bintangtobing.com>
2022-06-23 16:38:52 +0700

    update: minimize size of the each gallery

--- resources/views/home/galleries.blade.php
+++ resources/views/home/galleries.blade.php
@@ -9,71 +9,85 @@ peralatan, pabrik pemecah batu dan industri umum.')
     <div class="content-wrap">
         <div class="container clearfix">
             <div class="masonry-thumbs grid-container grid-5" data-big="1" data-lightbox="gallery">
-                <a class="grid-item" href="{!!asset('webpage/images/ems/palm-oil-mills/1.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/palm-oil-mills/1.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/palm-oil-mills/1-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/palm-oil-mills/1-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery palm oil 1"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/palm-oil-mills/2.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/palm-oil-mills/2.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/palm-oil-mills/2-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/palm-oil-mills/2-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery palm oil 2"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/palm-oil-mills/3.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/palm-oil-mills/3.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/palm-oil-mills/3-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/palm-oil-mills/3-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery palm oil 3"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/palm-oil-mills/4.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/palm-oil-mills/4.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/palm-oil-mills/4-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/palm-oil-mills/4-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery palm oil 4"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/palm-oil-mills/5.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/palm-oil-mills/5.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/palm-oil-mills/5-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/palm-oil-mills/5-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery palm oil 5"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/palm-oil-mills/6.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/palm-oil-mills/6.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/palm-oil-mills/6-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/palm-oil-mills/6-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery palm oil 6"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/palm-oil-mills/7.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/palm-oil-mills/7.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/palm-oil-mills/7-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/palm-oil-mills/7-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery palm oil 7"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/palm-oil-mills/8.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/palm-oil-mills/8.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/palm-oil-mills/8-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/palm-oil-mills/8-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery palm oil 9"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/1.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/stone-crusher-plans/1.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/1-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img
+                        src="{!!asset('webpage/images/ems/stone-crusher-plans/1-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery palm oil Stone Crusher Plan 1"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/2.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/stone-crusher-plans/2.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/2-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img
+                        src="{!!asset('webpage/images/ems/stone-crusher-plans/2-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery Stone Crusher Plan 2"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/3.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/stone-crusher-plans/3.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/3-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img
+                        src="{!!asset('webpage/images/ems/stone-crusher-plans/3-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery Stone Crusher Plan 3"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/4.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/stone-crusher-plans/4.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/4-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img
+                        src="{!!asset('webpage/images/ems/stone-crusher-plans/4-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery Stone Crusher Plan 4"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/5.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/stone-crusher-plans/5.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/5-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img
+                        src="{!!asset('webpage/images/ems/stone-crusher-plans/5-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery Stone Crusher Plan 5"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/6.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/stone-crusher-plans/6.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/6-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img
+                        src="{!!asset('webpage/images/ems/stone-crusher-plans/6-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery Stone Crusher Plan 6"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/7.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/stone-crusher-plans/7.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/7-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img
+                        src="{!!asset('webpage/images/ems/stone-crusher-plans/7-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery Stone Crusher Plan 7"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/8.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/stone-crusher-plans/8.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/8-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img
+                        src="{!!asset('webpage/images/ems/stone-crusher-plans/8-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery Stone Crusher Plan 8"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/9.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/stone-crusher-plans/9.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/9-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img
+                        src="{!!asset('webpage/images/ems/stone-crusher-plans/9-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery Stone Crusher Plan 9"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/10.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/stone-crusher-plans/10.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/10-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img
+                        src="{!!asset('webpage/images/ems/stone-crusher-plans/10-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery Stone Crusher Plan 10"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/11.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/stone-crusher-plans/11.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/11-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img
+                        src="{!!asset('webpage/images/ems/stone-crusher-plans/11-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery Stone Crusher Plan 11"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/12.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/stone-crusher-plans/12.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/12-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img
+                        src="{!!asset('webpage/images/ems/stone-crusher-plans/12-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery Stone Crusher Plan 12"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/13.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/stone-crusher-plans/13.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/13-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img
+                        src="{!!asset('webpage/images/ems/stone-crusher-plans/13-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery Stone Crusher Plan 13"></a>
-                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/14.jpg')!!}"
-                    data-lightbox="gallery-item"><img src="{!!asset('webpage/images/ems/stone-crusher-plans/14.jpg')!!}"
+                <a class="grid-item" href="{!!asset('webpage/images/ems/stone-crusher-plans/14-min.jpg')!!}"
+                    data-lightbox="gallery-item"><img
+                        src="{!!asset('webpage/images/ems/stone-crusher-plans/14-min.jpg')!!}"
                         alt="Elang Mas Surya Assets Gallery Stone Crusher Plan 14"></a>
             </div>
         </div>
