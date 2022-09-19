0de9ecc279c323705312bc3e09a186b051a4ef4f
bintangjtobing <hello@bintangtobing.com>
2022-09-19 13:31:17 +0700

    update: index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -101,6 +101,19 @@
         }
 
     </style>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-SSTDCEDK83"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-SSTDCEDK83');
+    </script>
+    
 </head>
 
 <body class="stretched">
